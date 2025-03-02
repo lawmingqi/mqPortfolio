d67aa1c898e7822ac816064a957e84f18cb9c06c
Ming Qi <s10257808@connect.np.edu.sg>
2025-03-02 20:16:49 +0800

    updated color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,13 +12,13 @@ const ServiceCard = ({ index, title, icon }) => {
     <Tilt className="xs: w-[250px] w-full">
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+        className="w-full red-yellow-gradient p-[1px] rounded-[20px] shadow-card">
           <div 
           options={{ max: 45,
             scale: 1,
             speed: 450
           }}
-          className ="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          className =" rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
             <img src={icon} alt={title} className="w-16 h-16 object-contain" />
             <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
           </div>
@@ -32,7 +32,7 @@ const About = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={`${styles.sectionHeadText} ${styles.transparentFill}`}>Overview.</h2>
+        <h2 className={`text-[#7A2A3D] font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]}`}>Overview.</h2>
       </motion.div>
 
       <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
@@ -41,7 +41,7 @@ const About = () => {
       With hands-on experience in modern web technologies, I continuously strive to enhance my skills and stay updated with industry trends.
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className="mt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
+        primary: "#F7F2E7",
+        secondary: "#7A2A3D",
         tertiary: "#151030",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
       },
       boxShadow: {
-        card: "0px 35px 120px -15px #211e35",
+        card: "0px 35px 120px -15px rgba(82, 82, 82, 0.74)",
       },
       screens: {
         xs: "450px",
